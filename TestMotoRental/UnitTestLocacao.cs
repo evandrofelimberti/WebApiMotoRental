a92f848c720ab5466446eb931e3b94f490fe8a42
evandrofelimberti <evandrofelimberti@gmail.com>
2024-05-20 19:52:33 -0300

    TestLocacaoCalculoPeriodoSuperior

--- TestMotoRental/UnitTestLocacao.cs
+++ TestMotoRental/UnitTestLocacao.cs
@@ -59,5 +59,32 @@ namespace MotoRentalTest
 
             Assert.Equal(valorAlugel, valorAluguelDiasExato);
         }
+
+        [Fact]
+        public void TestLocacaoCalculoPeriodoSuperior()
+        {
+            LocacaoDTO locacaoDTO = new LocacaoDTO();
+            locacaoDTO.DataInclusao = DateTime.Now;
+            locacaoDTO.DataInicio = locacaoDTO.DataInclusao.AddDays(1);
+            locacaoDTO.DataPrevisaoTermino = locacaoDTO.DataInclusao.AddDays(7);
+            locacaoDTO.DataTermino = locacaoDTO.DataInclusao.AddDays(9);
+            locacaoDTO.Descricao = "Teste locacao";
+            locacaoDTO.PessoaId = 0;
+            locacaoDTO.PlanoLocacaoId = 0;
+            locacaoDTO.QuantidadeDiasAluguel = 9;
+
+            Locacao locacao = new Locacao();
+            locacao.FromLocacaoDTO(locacaoDTO);
+            locacao.PlanoLocacao = new PlanoLocacao { Descricao = "Teste", QuantidadeDias = 7, PercentualMulta = 20, ValorDia = 50 };
+            Double valorAluguelDiasExato = 550;
+
+            var locacaoCalculoTipo = LocacaoCalculoTipo.PeriodoSuperiorTermino;
+
+            LocacaoCalculoFactory locacaoCalculoFactory = new LocacaoCalculoFactory();
+            var locacaoCalculo = locacaoCalculoFactory.CreateLocacaoCalculo(locacaoCalculoTipo);
+            var valorAlugel = locacaoCalculo.CalcularValor(locacao);
+
+            Assert.Equal(valorAlugel, valorAluguelDiasExato);
+        }
     }
 }
